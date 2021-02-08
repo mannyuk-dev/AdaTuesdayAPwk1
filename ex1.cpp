e6c2baef48847ce97a44b2fb33d3422e0658f231
mannyuk-dev <56549229+mannyuk-dev@users.noreply.github.com>
2021-02-08 05:35:20 +0000

    ex1 AP week1

---
+++ ex1.cpp
@@ -0,0 +1,23 @@
+//ex1 day2
+
+//
+#include <iostream>
+using namespace std;
+
+//Loop through each char from the end of original string and placinmg it in a new variable.
+int main() {
+
+    string greeting;
+    string new_greeting;
+    cout << "Please enter a word: ";
+    cin >> greeting;
+
+    for (int x = greeting.length() - 1; x >= 0; x--) {
+        new_greeting.push_back(greeting[x]);
+    }
+
+    cout <<  greeting << "spelt backwards is: " << new_greeting ;
+
+
+    return 0;
+}
